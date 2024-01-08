92bf749828c6b04f0e8e734a8edec0f0f80bb191
Andrew Harabor <andrew.harabor@gmail.com>
2024-01-07 18:23:17 -0800

    Finish documentation

--- src/gclib.h
+++ src/gclib.h
@@ -13,10 +13,10 @@ Initialize `gclib`.
 MUST BE CALLED FROM `main()` AND BEFORE THE PROGRAM USES ANY `gclib` FUNCTIONS.
 
 ### Description
-The main job of `gclib_init()` is to define important addresses in the processes' memory structure for the
-garbage collection step. If it is used incorrectly, the garbage collector will function as normal though will not be fully
-effective in freeing unreachable memory chunks. Note that if `gclib_init()` isn't called before a `gclib`
-function is used, the function will return immediately and return a null-value.
+`gclib_init()` defines important addresses in the processes' memory structure for the garbage collection step.
+If it is used incorrectly, the garbage collector will function as normal though will not be fully effective in freeing
+unreachable memory chunks. Note that if `gclib_init()` isn't called before a `gclib` function is used, the
+function will return immediately and return a null-value.
 
 ### Parameters
 None.
@@ -55,9 +55,9 @@ None.
 None.
 
 ### Return Value
-The return value is `true` if `gclib_init()` has been called but `gclib_cleanup()`, which means `gclib`
-functions can be called. Otherwise, `gclib_ready()` returns `false`. Note that the `bool` types are defined as
-`#define true 1` and `#define false 0` so the numerical literals can be interpreted instead.
+The return value is `true` if `gclib_init()` has been called but `gclib_cleanup()` has not, which means
+`gclib` functions can be called. Otherwise, `gclib_ready()` returns `false`. Note that the `bool` types are
+defined as `#define true 1` and `#define false 0` so the numerical literals can be interpreted instead.
 */
 bool gclib_ready(void);
 
@@ -92,7 +92,7 @@ void *gclib_alloc(size_t size, bool zeroed);
 Resize a chunk of dynamically allocated memory subject to garbage collection.
 
 ### Description
-`gclib_realloc()` acts as a wrapper around `realloc()` by passing the `ptr` and `new_size` argument to it.
+`gclib_realloc()` acts as a wrapper around `realloc()` by passing the `ptr` and `new_size` arguments to it.
 As such, it mirrors the behavior of this standard-library function. The result is stored internally for `gclib` to
 use later on during the garbage collection process.
 
@@ -113,15 +113,87 @@ that `realloc()` failed, even after running the garbage collector and trying aga
 */
 void *gclib_realloc(void *ptr, size_t new_size);
 
-/* Explicitly free the chunk pointed to by `ptr`, which should have been obtained through `gclib_alloc()` or `gclib_realloc()`. */
+/*
+### Synopsis
+Explicitly free a chunk of dynamically allocated memory subject to garbage collection.
+
+### Description
+`gclib_free()` acts as a warapper around `free()` by passing the argument `ptr` to it. As such, it mirrors the
+behavior of this standard-library function.
+
+### Parameters
+`ptr` - The pointer to the memory chunk that is to be freed. Note that `ptr` must have been returned by either
+`gclib_alloc()` or `gclib_realloc()`; any other pointer results in undefined and erroneous behavior.
+
+### Return Value
+None.
+*/
 void gclib_free(void *ptr);
 
-/* Explicitly run the garbage collector to free unreachable memory obtained through `gclib_alloc()` or `gclib_realloc()`; will have a limited effect if insufficient allocations have been accumulated. */
+/*
+### Synopsis
+Explicitly run the garbage collector in order to free unreachable dynamically allocated memory chunks.
+
+### Description
+`gclib_collect()` runs a standard cycle of the garbage collector in an effort to collect memory chunks allocated
+through `gclib_alloc()` and `gclib_realloc()`. However, note that because each generation of chunks is only
+swept through after a certain number of bytes have been allocated, not much will be collected if only a small
+amount of memory has been allocated through `gclib` functions.
+
+### Parameters
+None.
+
+### Return Value
+None.
+*/
 void gclib_collect(void);
 
-/* Force the garbage collector to free unreachable memory obtained through `gclib_alloc()` or `gclib_realloc()`, running a cycle through all allocations. */
+/*
+### Synopsis
+Forcibly run the garbage collector in order to free unreachable dynamically allocated memory chunks from all
+generations.
+USE OF THIS FUNCTION IS NOT RECOMMENDED AS IT IS TIME CONSUMING AND MAY INTERFERE WITH THE
+COLLECTOR'S NATURAL CYCLES.
+
+### Description
+`gclib_force_collect()` runs a cycle of the garbage collector but with the condition that all generations are
+swept through. The result is that any chunks allocated through `gclib_alloc()` and `gclib_realloc()` that are
+determined to be unreachable are freed.
+
+### Parameters
+None.
+
+### Return Value
+None.
+*/
 void gclib_force_collect(void);
 
+/*
+### Synopsis
+Print out any unfreed memory chunks allocated through `gclib_alloc()` and `gclib_realloc()`.
+
+### Description
+`gclib_print_leaks()` is intended to utilize `gclib`'s tracking of memory for garbage collection as part of
+memory-leak detection capability. Note that throughout the duration of the program, as allocations are made, the
+collector may activate and free unreachable chunks. This means that the diagnosis of memory leaks may not be
+completely accurate. Nevertheless, it may give the caller a decent idea of where the program leaks memory. In a
+program that uses only the standard-library allocation functions, the following macros can be used in conjunction with
+`gclib_print_leaks()` to identify unfreed memory chunks (of course `gclib_init()` and `gclib_cleanup()` should be
+included as well).
+
+``` c
+#define malloc(size) gclib_alloc(size, false)
+#define calloc(count, size_each) gclib_alloc((count) * (size_each), true)
+#define realloc(ptr, new_size) gclib_realloc(ptr, new_size)
+#define free(ptr) gclib_free(ptr)
+```
+
+### Parameters
+`stream` - The file or output stream in which to print the found memory leaks.
+
+### Return Value
+None.
+*/
 void gclib_print_leaks(FILE *stream);
 
 
